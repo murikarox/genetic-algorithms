6134d541ffddf7d039217542c870c57d099adbaf
Murat Eksi <murat@eksiventures.com>
2024-07-22 17:07:57 +0200

    add single point crossover function

--- genetic/lib/toolbox/crossover.ex
+++ genetic/lib/toolbox/crossover.ex
@@ -33,4 +33,12 @@ defmodule Toolbox.Crossover do
        size: p2.size
      }}
   end
+
+  def single_point(p1, p2) do
+    cx_point = :rand.uniform(p1.size)
+    {p1_head, p1_tail} = Enum.split(p1.genes, cx_point)
+    {p2_head, p2_tail} = Enum.split(p2.genes, cx_point)
+    {c1, c2} = {p1_head ++ p2_tail, p2_head ++ p1_tail}
+    {%Chromosome{genes: c1, size: length(c1)}, %Chromosome{genes: c2, size: length(c2)}}
+  end
 end
