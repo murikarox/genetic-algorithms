d7a1e79b36a1f441e49a4c5e264059c73d193201
Murat Eksi <murat@eksiventures.com>
2024-07-22 17:28:43 +0200

    add chromosome repair functionality

--- genetic/lib/genetic.ex
+++ genetic/lib/genetic.ex
@@ -43,6 +43,13 @@ defmodule Genetic do
       {c1, c2} = apply(crossover_fn, [p1, p2])
       [c1, c2 | acc]
     end)
+    |> Enum.map(&repair_chromosome(&1))
+  end
+
+  def repair_chromosome(chromosome) do
+    genes = MapSet.new(chromosome.genes)
+    new_genes = repair_helper(chromosome, 8)
+    %Chromosome{chromosome | genes: new_genes}
   end
 
   def mutation(population, opts \\ []) do
@@ -81,6 +88,15 @@ defmodule Genetic do
       (children ++ leftover)
       |> mutation(opts)
       |> evolve(problem, generation, best.fitness, temperature, opts)
+    end
+  end
+
+  defp repair_helper(chromosome, k) do
+    if MapSet.size(chromosome) >= k do
+      MapSet.to_list(chromosome)
+    else
+      num = :rand.uniform(8)
+      repair_helper(MapSet.put(chromosome, num), k)
     end
   end
 end
